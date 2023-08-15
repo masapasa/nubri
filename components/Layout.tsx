cd870bb78575de0a5ec4230423f43f98a8993b2c
masapasa <apyakurel@gmail.com>
2023-08-15 13:51:07 +0200

    chat

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -73,6 +73,16 @@ export default function Layout({ children }: { children: any }) {
                       </Link>
                     </li>
                     <li>
+                      <Link href="https://chat.nubri.co/">
+                        <button
+                          className="p-2 rounded-lg hover:bg-gray-200 hover:dark:bg-[#1d1f22] hover:ring-2 ring-gray-300 transition-all"
+                          onClick={toggleNavbar}
+                        >
+                          Chat
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
                       <Link href="/gallery">
                         <button
                           className="p-2 rounded-lg hover:bg-gray-200 hover:dark:bg-[#1d1f22] hover:ring-2 ring-gray-300 transition-all"
